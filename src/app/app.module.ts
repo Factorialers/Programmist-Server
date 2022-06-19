100ca5e5df50a22f873628f4d5b4be2bc7032a1e
dino3616 <suraimu3616@outlook.jp>
2022-06-19 23:22:13 +0900

    feat: ✨ firebaseに関する処理をModule化
    テスタブルなコードにするために、firebaseに関する処理をModule化してDIを可能にした。
    また、その影響でimportパスとDIを一部修正した。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import Components from '../components';
 import EnvModule from '../config/environment/getter/getter.module';
 import GraphQLConfig from '../config/graphql/graphql.config';
+import FirebaseModule from '../libs/firebase/firebase.module';
 import PrismaModule from '../libs/prisma/prisma.module';
 
 @Module({
-  imports: [EnvModule, GraphQLConfig, PrismaModule, ...Components],
+  imports: [EnvModule, GraphQLConfig, PrismaModule, FirebaseModule, ...Components],
 })
 export default class AppModule {}
