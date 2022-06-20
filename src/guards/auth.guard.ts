b2c73deff327b78c73d19bc1a063b43473a8dc2a
dino3616 <suraimu3616@outlook.jp>
2022-06-20 09:48:40 +0900

    feat: 💡 認可処理をよりセキュアに修正
    tokenの存在確認のみをしていた処理に、存在確認できたtokenを持つuidが渡されたuidと一致するかを確認する処理を追加した。

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -2,14 +2,14 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import FirebaseService from '../libs/firebase/firebase.service';
 
-const validateAuthorization = async (firebase: FirebaseService, authorization: string | undefined) => {
-  if (!authorization) {
+const validateAuthorization = async (firebase: FirebaseService, authorization: string | undefined, uid: string | undefined) => {
+  if (!(authorization && uid)) {
     return false;
   }
 
   const decodedToken = await firebase.adminAuth.verifyIdToken(authorization);
 
-  return !!decodedToken;
+  return decodedToken.uid === uid;
 };
 
 @Injectable()
@@ -18,13 +18,20 @@ export default class AuthGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     let authorization: string | undefined;
+    let uid: string | undefined;
     context.getArgs().forEach((arg) => {
       if (arg && arg.authorization) {
         authorization = arg.authorization;
       }
+      if (arg && arg.data && arg.data.id) {
+        uid = arg.data.id;
+      }
+      if (arg && arg.where && arg.where.id) {
+        uid = arg.where.id;
+      }
     });
 
-    const isValid = validateAuthorization(this.firebase, authorization);
+    const isValid = validateAuthorization(this.firebase, authorization, uid);
 
     return isValid;
   }
