2561c05c08c3eb7e11b44a8ec251af46f0b5e278
dino3616 <suraimu3616@outlook.jp>
2022-07-03 14:04:12 +0900

    ci: 🦺 Workの単体テストを実装

---
+++ src/components/work/work.resolver.mutation.spec.ts
@@ -0,0 +1,108 @@
+import { mock, MockedType } from '../../test/mock';
+import WorkMutation from './work.resolver.mutation';
+import WorkService from './work.service';
+
+describe('Work Mutation Resolver Test', () => {
+  let mockedWorkService: MockedType<WorkService>;
+  let workQuery: WorkMutation;
+
+  beforeEach(() => {
+    mockedWorkService = mock<WorkService>();
+    workQuery = new WorkMutation(mockedWorkService);
+  });
+
+  test('createWork', async () => {
+    const fakeWork = {
+      id: 'abc-123',
+      title: 'yukarisan-ga-kawaii-hanasi',
+      tags: ['yukarisan', 'kawaii'],
+      logoURL: 'logo-url',
+      readmeURL: 'readme-url',
+      sourceCodeURL: ['src/main.ts', 'src/libs/index.ts'],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      publishedAt: null,
+      isPublish: false,
+      userId: 'yukarisan-id',
+    };
+    const fakeUser = {
+      id: 'yukarisan-id',
+      name: 'yukarisan-lover',
+      works: [fakeWork],
+      createdAt: new Date(),
+    };
+    const fakeWorkWithUser = {
+      ...fakeWork,
+      user: fakeUser,
+    };
+
+    mockedWorkService.create.mockResolvedValue(fakeWorkWithUser);
+
+    const expectWork = fakeWorkWithUser;
+    await expect(workQuery.createWork({ data: expectWork })).resolves.toEqual(expectWork);
+  });
+
+  test('updateWork', async () => {
+    const fakeWork = {
+      id: 'abc-123',
+      title: 'yukarisan-ga-kawaii-hanasi',
+      tags: ['yukarisan', 'kawaii'],
+      logoURL: 'logo-url',
+      readmeURL: 'readme-url',
+      sourceCodeURL: ['src/main.ts', 'src/libs/index.ts'],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      publishedAt: null,
+      isPublish: false,
+      userId: 'yukarisan-id',
+    };
+    const fakeUser = {
+      id: 'yukarisan-id',
+      name: 'yukarisan-lover',
+      works: [fakeWork],
+      createdAt: new Date(),
+    };
+    const fakeWorkWithUser = {
+      ...fakeWork,
+      user: fakeUser,
+    };
+
+    mockedWorkService.update.mockResolvedValue(fakeWorkWithUser);
+
+    const expectWork = fakeWorkWithUser;
+    await expect(
+      workQuery.updateWork({ where: { id: expectWork.id, userId: expectWork.userId }, data: { title: expectWork.title } }),
+    ).resolves.toEqual(expectWork);
+  });
+
+  test('deleteWork', async () => {
+    const fakeWork = {
+      id: 'abc-123',
+      title: 'yukarisan-ga-kawaii-hanasi',
+      tags: ['yukarisan', 'kawaii'],
+      logoURL: 'logo-url',
+      readmeURL: 'readme-url',
+      sourceCodeURL: ['src/main.ts', 'src/libs/index.ts'],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      publishedAt: null,
+      isPublish: false,
+      userId: 'yukarisan-id',
+    };
+    const fakeUser = {
+      id: 'yukarisan-id',
+      name: 'yukarisan-lover',
+      works: [fakeWork],
+      createdAt: new Date(),
+    };
+    const fakeWorkWithUser = {
+      ...fakeWork,
+      user: fakeUser,
+    };
+
+    mockedWorkService.delete.mockResolvedValue(fakeWorkWithUser);
+
+    const expectWork = fakeWorkWithUser;
+    await expect(workQuery.deleteWork({ where: { id: expectWork.id, userId: expectWork.userId } })).resolves.toEqual(expectWork);
+  });
+});
